ccb8ae104c595a449f529e61692e43d81f1b03ba
DylanCheetah <dylan_the_cheetah@outlook.com>
2022-09-05 17:10:52 -0400

    Updated camera system.

--- addons/Cybermals-EagleEye/FlyCam.gd
+++ addons/Cybermals-EagleEye/FlyCam.gd
@@ -38,19 +38,19 @@ func _fixed_process(delta):
 		move_vec.y = -1
 		
 	if Input.is_action_pressed("look_left"):
-		rotation.y += -look_speed * delta
-		
-	elif Input.is_action_pressed("look_right"):
 		rotation.y += look_speed * delta
 		
+	elif Input.is_action_pressed("look_right"):
+		rotation.y += -look_speed * delta
+		
 	if Input.is_action_pressed("look_up"):
-		rotation.x += -look_speed * delta
+		rotation.x += look_speed * delta
 		
 	elif Input.is_action_pressed("look_down"):
-		rotation.x += look_speed * delta
+		rotation.x += -look_speed * delta
 		
 	#Update movement
 	set_rotation(Vector3())
-	rotate_y(deg2rad(rotation.y))
-	rotate_x(deg2rad(rotation.x))
-	set_translation(get_translation() + move_vec.rotated(Vector3(0.0, 1.0, 0.0), deg2rad(rotation.y)) * move_speed * (slowdown_factor if Input.is_action_pressed("move_slow") else 1.0) * delta)
+	rotate_y(-deg2rad(rotation.y))
+	rotate_x(-deg2rad(rotation.x))
+	set_translation(get_translation() + move_vec.rotated(Vector3(0.0, 1.0, 0.0), -deg2rad(rotation.y)) * move_speed * (slowdown_factor if Input.is_action_pressed("move_slow") else 1.0) * delta)
